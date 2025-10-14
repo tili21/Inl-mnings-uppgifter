6e0b320969a0a4ddd60ce60595630962954a57ac
tili21 <tili08001@utb.vaxjo.se>
2025-10-14 12:21:58 +0200

    Add files via upload

---
+++ Program (1).cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BlueScreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.WriteLine("Ett allvarligt systemfel har inträffat.");
+            Console.WriteLine("Datorn måste startas om.");
+
+       
+        }
+    }
+}
