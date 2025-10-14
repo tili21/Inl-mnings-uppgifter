de5823572239f6c71cd2b685e6d329d1f49cb815
tili21 <tili08001@utb.vaxjo.se>
2025-10-14 12:22:29 +0200

    Add files via upload

---
+++ Program (2).cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Triangel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    /\\\"");
+            Console.WriteLine("   /  \\\"");
+            Console.WriteLine("  / \"\" \\\"");
+            Console.WriteLine(" /______\\\"");
+        }
+    }
+}
