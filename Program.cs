3029f6fa80e3cf8ab135f72317866dea8f88f2d4
tili21 <tili08001@utb.vaxjo.se>
2025-10-14 12:21:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TEST2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\.");
+        }
+    }
+}
+
